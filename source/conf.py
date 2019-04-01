947c6f0960e42a5e361e4be1d1727bb01d155643
Jasper Jonker <j8tjuh@gmail.com>
2019-04-02 00:14:56 +0200

    new

--- source/conf.py
+++ source/conf.py
@@ -49,6 +49,7 @@ exclude_patterns = []
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_templates", ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
